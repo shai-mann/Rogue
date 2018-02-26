9307c738f860c5189fefc8a7f6b8d793275020f9
Shai Mann-Robison <mann.robison@gmail.com>
2018-02-26 15:45:29 -0500

    Create Simple Room Generation

--- src/Room.java
+++ src/Room.java
@@ -5,6 +5,8 @@ import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Room {
 
@@ -21,6 +23,9 @@ public class Room {
     private int yLength;
 
     public Room(int x, int y) {
+        xLength = x;
+        yLength = y;
+
         setDefaults();
 
         GameManager.replaceContentPane(panel);
@@ -38,14 +43,37 @@ public class Room {
                 TitledBorder.DEFAULT_POSITION,
                 null, Helper.FOREGROUND_COLOR
         ));
+
+        table.setBackground(Helper.BACKGROUND_COLOR);
     }
     public void add(JComponent c, int x, int y) {
         //adds component to table at x and y coords given
     }
     private void createUIComponents() {
         // TODO: place custom component creation code here
-        //the number of rows is the x value, y is columns
 
-        table = new JTable();
+        DefaultTableModel model = createTableModel();
+        table = new JTable(model);
+    }
+    public String[] getDefaultRow(String edges, String rests) {
+        String[] rowValueList = new String[xLength];
+        Arrays.fill(rowValueList, rests);
+        rowValueList[0] = edges;
+        rowValueList[xLength - 1] = edges;
+        return rowValueList;
+    }
+    public DefaultTableModel createTableModel() {
+        DefaultTableModel model = new DefaultTableModel();
+        for (int x = 0; x < xLength; x++) {
+            model.addColumn("col" + x);
+            table.
+        }
+        String[] rowValueList = getDefaultRow("|", "-");
+        model.addRow(getDefaultRow("_", "_"));
+        for (int y = 0; y < yLength - 2; y++) {
+            model.addRow(rowValueList);
+        }
+        model.addRow(getDefaultRow("_", "_"));
+        return model;
     }
 }
