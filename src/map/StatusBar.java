df523176f20d62355bb6fa5f72aecaac9ca29b37
Shai Mann-Robison <mann.robison@gmail.com>
2018-03-16 09:02:29 -0400

    Add onto outline for statusBar

--- src/map/StatusBar.java
+++ src/map/StatusBar.java
@@ -1,18 +1,14 @@
 package map;
 
-import entity.Player;
 import entity.monster.Monster;
 import helper.Helper;
 import main.GameManager;
 
 import javax.swing.*;
-import java.lang.management.PlatformLoggingMXBean;
 
 public class StatusBar extends JComponent {
     private JLabel healthField;
     private JPanel panel;
-
-    private Player player;
 
     // This class is what takes the player's stats and displays them at the bottom of the screen
     public StatusBar() {
