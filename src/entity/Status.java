8f6fe8d178df09214bbfa09391cb31f4c33f8686
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-03-22 12:54:40 -0400

    Create Status class for living entities

---
+++ src/entity/Status.java
@@ -0,0 +1,48 @@
+package entity;
+
+public class Status {
+
+    private int health = 20;
+    private int paralyzed = 0;
+    private int confused = 0;
+    private int drunk = 0;
+
+    public Status() {
+
+    }
+
+    public void update() {
+        if (isParalyzed()) {
+            this.paralyzed -= 1;
+        }
+        if (isConfused()) {
+            this.confused -= 1;
+        }
+        if (isDrunk()) {
+            this.drunk -= 1;
+        }
+    }
+
+    public int getHealth() {
+        return health;
+    }
+    public boolean isParalyzed() {
+        return paralyzed > -1;
+    }
+    public boolean isConfused() {
+        return confused > 0;
+    }
+    public boolean isDrunk() {
+        return drunk > 0;
+    }
+    public void setParalyzed(int turns) {
+        this.paralyzed = turns;
+    }
+    public void setConfused(int turns) {
+        this.confused = turns;
+    }
+    public void setDrunk(int drunk) {
+        this.drunk = drunk;
+    }
+
+}
