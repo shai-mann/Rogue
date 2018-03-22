14084b95cd4b6ec47b124373653948efb5a3befb
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-03-22 12:54:54 -0400

    Use Status in Player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -7,43 +7,49 @@ import map.Map;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class Player extends Entity implements KeyListener {
 
     private Map map;
+    private Status status;
 
     public Player() {
         super("@", 5, 5);
         GameManager.getFrame().addKeyListener(this);
-
+        status = new Status();
         map = Map.getMap();
     }
 
     public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_W:
-            case KeyEvent.VK_UP:
-                move(UP);
-                map.update();
-                break;
-            case KeyEvent.VK_S:
-            case KeyEvent.VK_DOWN:
-                move(DOWN);
-                map.update();
-                break;
-            case KeyEvent.VK_A:
-            case KeyEvent.VK_LEFT:
-                move(LEFT);
-                map.update();
-                break;
-            case KeyEvent.VK_D:
-            case KeyEvent.VK_RIGHT:
-                move(RIGHT);
-                map.update();
-                break;
-            default:
-                break;
+        if (!status.isParalyzed() && !status.isConfused()) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_W:
+                case KeyEvent.VK_UP:
+                    move(UP);
+                    break;
+                case KeyEvent.VK_S:
+                case KeyEvent.VK_DOWN:
+                    move(DOWN);
+                    break;
+                case KeyEvent.VK_A:
+                case KeyEvent.VK_LEFT:
+                    move(LEFT);
+                    break;
+                case KeyEvent.VK_D:
+                case KeyEvent.VK_RIGHT:
+                    move(RIGHT);
+                    break;
+                default:
+                    break;
+            }
         }
+        if (status.isConfused() || status.isDrunk()) {
+            int[] directions = {UP,DOWN,RIGHT,LEFT};
+            move(directions[new Random().nextInt(directions.length)]);
+        }
+        status.update();
+        map.update();
     }
     public void keyReleased(KeyEvent e) {}
     public void keyTyped(KeyEvent e) {}
@@ -57,5 +63,8 @@ public class Player extends Entity implements KeyListener {
     }
     private boolean checkDeath() {
         return getHealth() == 0;
+    }
+    public Status getStatus() {
+        return status;
     }
 }
