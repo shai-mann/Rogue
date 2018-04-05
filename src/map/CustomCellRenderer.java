528fa483939567ce855b339d24baa6557008be10
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-04-05 14:58:35 -0400

    Color player and monster entities

--- src/map/CustomCellRenderer.java
+++ src/map/CustomCellRenderer.java
@@ -6,16 +6,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CustomCellRenderer extends DefaultTableCellRenderer {
-    @Override
+
     public Component getTableCellRendererComponent (
             JTable table, Object obj, boolean isSelected, boolean hasFocus, int row, int column
         ) {
         Component cell = super.getTableCellRendererComponent(
                 table, obj, isSelected, hasFocus, row, column);
-
         cell.setBackground(Helper.BACKGROUND_COLOR);
-        cell.setForeground(Helper.FOREGROUND_COLOR);
+        cell.setForeground(getForegroundColor((String) table.getModel().getValueAt(row, column)));
 
         return cell;
+    }
+    private Color getForegroundColor(String value) {
+        if (value == "@") {
+            return Color.YELLOW;
+        } else if (value.matches("[a-zA-Z]")) { // monsters
+            return Color.RED;
+        } else {
+            return Helper.FOREGROUND_COLOR;
+        }
     }
 }
