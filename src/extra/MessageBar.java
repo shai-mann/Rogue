ad2b1b1228617a6fc097b4d2ac1c7fb874456a8e
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-04-11 16:06:04 -0400

    Create basic message bar

---
+++ src/extra/MessageBar.java
@@ -0,0 +1,61 @@
+package extra;
+
+import helper.Helper;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MessageBar extends JComponent {
+
+    private JPanel panel;
+    private JTextArea message;
+    private JTextArea oldMessage;
+    private JTextArea olderMessage;
+    private static int index = 0;
+
+    private static MessageBar messageBar;
+
+    public MessageBar() {
+        super();
+        messageBar = this;
+        setDefaults();
+    }
+    public void setDefaults() {
+        panel.setBackground(Helper.BACKGROUND_COLOR);
+        panel.setForeground(Helper.BACKGROUND_COLOR);
+        setTextareaDefaults(message, Helper.FOREGROUND_COLOR);
+        setTextareaDefaults(oldMessage, Helper.changeOpacity(Helper.FOREGROUND_COLOR, 95));
+        setTextareaDefaults(olderMessage, Helper.changeOpacity(Helper.FOREGROUND_COLOR, 90));
+    }
+    private void setTextareaDefaults(JTextArea textArea, Color foreground) {
+        textArea.setBackground(Helper.BACKGROUND_COLOR);
+        textArea.setForeground(foreground);
+        textArea.setFont(new Font(Helper.THEME_FONT, Font.BOLD, 12));
+        textArea.setFocusable(false);
+    }
+    private void _addMessage(String text) {
+        String currentMessage = message.getText();
+        if (!currentMessage.trim().equals("")) {
+            message.setText(currentMessage + "; " + text);
+        } else {
+            message.setText(text);
+        }
+    }
+    private void _nextTurn() {
+        String currentMessage = message.getText();
+        String currentOldMessage = oldMessage.getText();
+
+        message.setText("");
+        oldMessage.setText(currentMessage);
+        olderMessage.setText(currentOldMessage);
+
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    public static void addMessage(String text) { messageBar._addMessage(text); }
+    public static void nextTurn() {
+        messageBar._nextTurn();
+    }
+
+}
