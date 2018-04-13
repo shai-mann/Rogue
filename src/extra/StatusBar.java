faa740cc42c09d8068d486767b1bea760d6de931
Shai Mann-Robison <mann.robison@gmail.com>
2018-04-13 09:12:06 -0400

    Add Gold to StatusBar

--- src/extra/StatusBar.java
+++ src/extra/StatusBar.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 public class StatusBar extends JComponent {
     private JLabel healthField;
     private JPanel panel;
+    private JLabel goldField;
 
     // This class is what takes the player's stats and displays them at the bottom of the screen
     public StatusBar() {
@@ -21,9 +22,12 @@ public class StatusBar extends JComponent {
 
         healthField.setText("Health: " + Monster.DEFAULT_HEALTH);
         healthField.setForeground(Helper.FOREGROUND_COLOR);
+        goldField.setText("Gold: 0");
+        goldField.setForeground(Helper.FOREGROUND_COLOR);
     }
     public void updateStatusBar() {
         healthField.setText("Health: " + GameManager.getPlayer().getHealth());
+        goldField.setText("Gold: " + GameManager.getPlayer().getGold());
         panel.revalidate();
         panel.repaint();
     }
