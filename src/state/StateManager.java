fbb125151c14f829ac6b2890a2e48e4b42761230
Shai Mann-Robison <mann.robison@gmail.com>
2022-06-29 17:34:31 -0400

    Add StateUpdate and StateManager classes

---
+++ src/state/StateManager.java
@@ -0,0 +1,35 @@
+package state;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+/**
+ * The StateManager manages updating and rendering the game. It is given {@link StateUpdate} objects
+ * on update that indicate which components to update before rendering.
+ */
+public class StateManager {
+
+    enum Update {
+        PLAYER_GUI,
+        PLAYER_STATUS,
+        PLAYER_HUNGER,
+        PLAYER_REGEN,
+        //PLAYER_MANA,
+        MONSTERS,
+        ITEMS,
+        CHAT,
+        ANIMATIONS,
+    }
+
+    private final Map<Update, Consumer<StateUpdate>> hooksMap = new HashMap<>();
+
+    public void update(StateUpdate state) {
+        state.updates.forEach(u -> hooksMap.getOrDefault(u, u1 -> {}).accept(state));
+    }
+
+    public void addHook(Update update, Consumer<StateUpdate> consumer) {
+        hooksMap.put(update, consumer);
+    }
+
+}
---
+++ src/state/StateUpdate.java
@@ -0,0 +1,32 @@
+package state;
+
+import state.StateManager.Update;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * A StateUpdate represents an update to the game state. Various types of updates can occur,
+ * and those can cause various subsets of the components of the game to update/refresh. This class
+ * is initialized with values to indicate which subset should update.
+ *
+ * @see StateManager
+ */
+public class StateUpdate {
+
+    public static final StateUpdate GAME_TICK = new StateUpdate();
+
+    final Set<Update> updates = Arrays.stream(Update.values()).collect(Collectors.toSet());
+
+    public StateUpdate() {}
+
+    public void add(Update update) {
+        updates.add(update);
+    }
+
+    public void remove(Update update) {
+        updates.remove(update);
+    }
+
+}
